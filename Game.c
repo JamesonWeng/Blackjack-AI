9f258c8ecb489bfe62a4e331e4b1cfc393041a9f
Jameson Weng <weng.jameson126@gmail.com>
2016-01-17 01:17:01 -0500

    Started hash table implementation for efficient AI response lookup

--- Game.c
+++ Game.c
@@ -1,119 +1,74 @@
-// creation of AI through genetic algorithm for a simple single-deck version of blackjack:
+// Creation of AI through genetic algorithm for a single-deck version of blackjack
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
 #include <time.h>
 
 #define NUM_RANKS 13
 #define NUM_SUITS 4
 #define DECK_SIZE NUM_RANKS * NUM_SUITS
+#define NUM_SHUFFLE 200 	// number of swaps used to shuffle the deck
+
 #define INITIAL_HAND_SIZE 2
-#define MAX_HAND_SIZE 12 // for a single deck, it's impossible to go over 12 cards without busting
-#define NUM_SHUFFLE 200
-#define NUM_GENERATIONS 1000 // number of times to repeat the "natural selection" for the genetic algorithm
-#define NUM_GAMES_PER_EVALUATION 100
+#define MAX_HAND_SIZE 12 	// for a single deck, it's impossible to go over 12 cards in a hand without busting
+
+#define NUM_GENERATIONS 1000 	// number of times to repeat the "natural selection" for the genetic algorithm
+#define NUM_GAMES_PER_EVALUATION 100	// number of games used to evaluate fitness
+#define POOL_SIZE 100	// number of members in each generation
+
+#define HASH_ARRAY_SIZE 55	// the number of two card combinations when treating ten, jack, queen, and king
+							// as the same type of card, since they hold the same value in blackjack
 
 typedef struct {
 	int rank, suit;
 } cardType;
 
 typedef struct {
-	int handSize; // current hand size
-	cardType *cards[MAX_HAND_SIZE]; // points to cards in the deck
+	int sum, handSize;	// key used to lookup the hand in the hash table
+	cardType *cards[MAX_HAND_SIZE];	// array that points to cards
 } handType;
 
-// hash table for efficient lookup of the AI responses
-// the keys will correspond the hand composition of the AI
-// the values will be the response (0 to stand, 1 to hit)
+struct nodeType {
+	long long int key;
+	int value;	// value is the response of the AI
+	struct nodeType *next;
+};
+typedef struct nodeType nodeType;
 
-
+// Hash table for efficient lookup of the AI's responses: the keys will correspond the hand composition of the AI, and
+// and the values will be the response (0 to stand, 1 to hit). The hash table is implemented as an array of linked lists.
+typedef struct {
+	nodeType *heads[HASH_ARRAY_SIZE];
+} hashTableType;
 
 typedef struct {
 	handType hand;
-	int fitness;
-	int response[];
+	int fitness;	// measure of how good the AI is
+	hashTableType response;
 } AIType;
 
 
+/********************************************
+ 	 MISCELLANEOUS HELPING FUNCTIONS
+ ********************************************/
+
+// randInt: returns a random integer between min and max inclusive
 int randInt (int min, int max) {
 	return rand() % (max - min + 1) + min;
 }
 
-/****************************
- ********************************************
- FUNCTIONS FOR THE CARD HAND
- ********************************************
- ****************************/
-
-// inserts a card into an array of cards sorted in increasing order by rank
-void insert (cardType *card, cardType **cards, int handSize) {
-	int place = 0;
-	while (cards[place]->rank < card->rank && place < handSize) {
-		place += 1;
-	}
-
-	for (int i = handSize + 1; i > place; i--) {
-		cards[i] = cards[i-1];
-	}
-
-	cards[place] = card;
-}
-
-// finds the largest sum under 21, it it's possible, for a given hand
-int bestSum (cardType **cards, int handSize) {
-	int numAces = 0;
-	int sum = 0;
-
-	// find the sum of the hand assuming the aces are 1
-	for (int i = 0; i < handSize; i++) {
-		if (cards[handSize]->rank == 0) {
-			numAces += 1;
-		}
-		if (cards[handSize]->rank >= 0 && cards[handSize]->rank <= 9) {
-			sum += cards[handSize]->rank + 1;
-		}
-		else {
-			sum += 10;
-		}
-	}
-
-	// find the best possible sum (without busting) by changing the worth of the aces
-	while (numAces > 0  && sum <= 11) {
-		sum += 10;
-		numAces -= 1;
-	}
-
-	return sum;
-}
-
-/****************************
- ********************************************
- FUNCTIONS FOR THE AI
- ********************************************
- ****************************/
-
-int calculateFitness (AIType *AI) {
-	handType dealer;
-	for (int i = 0; i < NUM_GAMES_PER_EVALUATION; i++) {
-
-
-
-	}
-}
-
-
-/****************************
- ********************************************
- FUNCTIONS FOR THE DECK
- ********************************************
- ****************************/
+/********************************************
+ 	 FUNCTIONS FOR A DECK
+ ********************************************/
 
 void createDeck (cardType **deck) {
 	for (int i = 0; i < NUM_RANKS; i++) {
 		for (int j = 0; j < NUM_SUITS; j++) {
 			deck[i*NUM_SUITS + j] = malloc (sizeof (cardType));
+			if (deck[i*NUM_SUITS + j] == NULL) {
+				printf ("Memory allocation for deck failed \n");
+			}
 			deck[i*NUM_SUITS + j]->rank = i;
 			deck[i*NUM_SUITS + j]->suit = j;
 		}
@@ -134,32 +89,245 @@ void shuffleDeck (cardType **deck) {
 	}
 }
 
-// for debugging purposes
-void printDeck (cardType **deck) {
+void freeDeck (cardType **deck) {
 	for (int i = 0; i < DECK_SIZE; i++) {
-		printf ("%i %i\n", deck[i]->rank, deck[i]->suit);
+		free (deck[i]);
+	}
+}
+
+// printCards: writes the contents of an array of card pointers to standard output - for debugging purposes
+void printCards (cardType **cards, int numCards) {
+	for (int i = 0; i < numCards; i++) {
+		printf ("%i %i\n", cards[i]->rank, cards[i]->suit);
+	}
+}
+
+/********************************************
+ 	 FUNCTIONS FOR THE CARD HAND
+ ********************************************/
+
+// handInsert: inserts a card into an array of cards sorted in increasing order by rank, and increases handSize by 1
+void handInsert (handType *hand, cardType *card) {
+	int place = 0;
+	while (place < hand->handSize && hand->cards[place]->rank <= card->rank) {
+		place += 1;
+	}
+
+	for (int i = hand->handSize; i > place; i--) {
+		hand->cards[i] = hand->cards[i-1];
+	}
+
+	hand->cards[place] = card;
+	hand->handSize += 1;
+}
+
+// handRemove: removes a card from the hand
+void handRemove (handType *hand, cardType *card) {
+
+	int place = 0;
+	while (place < hand->handSize && hand->cards[place]->rank != card->rank) {
+		place += 1;
+	}
+
+	hand->handSize -= 1;
+	for (int i = place; i < hand->handSize; i++) {
+		hand->cards[i] = hand->cards[i+1];
+	}
+}
+
+
+// handFindSum: tries to find the largest sum under 21 for a given hand (if the hand is a bust, the sum will be over 21)
+int handFindSum (handType *hand) {
+	int numAces = 0;
+	int sum = 0;
+
+	// find the sum of the hand assuming the aces are 1
+	for (int i = 0; i < hand->handSize; i++) {
+		if (hand->cards[i]->rank == 0) {
+			numAces += 1;
+		}
+
+		if (hand->cards[i]->rank >= 0 && hand->cards[i]->rank <= 9) {
+			sum += hand->cards[i]->rank + 1;
+		}
+		else {
+			sum += 10;
+		}
+	}
+
+	// find the best possible sum (without busting) by changing the worth of the aces
+	while (numAces > 0  && sum <= 11) {
+		sum += 10;
+		numAces -= 1;
+	}
+
+	return sum;
+}
+
+/********************************************
+ 	 HASH TABLE FUNCTIONS
+ ********************************************/
+
+nodeType* listInsert (nodeType *head, long long int key, int value) {
+	if (head == NULL) {
+		head = malloc(sizeof(nodeType));
+		if (head == NULL) {
+			printf ("Memory allocation for head failed\n");
+		}
+		head->key = key;
+		head->value = value;
+		head->next = NULL;
+	}
+	else if (head->key != key) {
+		head->next = listInsert (head->next, key, value);
+	}
+
+	return head;
+}
+
+// handToKey: computes the key for a given hand
+long long int handToKey (handType *hand) {
+	long long int key = 0;
+
+	for (int i = 0; i < hand->handSize; i++) {
+		if (hand->cards[i]->rank < 9) {
+			key = key * 10 + hand->cards[i]->rank;
+		}
+		else {
+			key = key * 10 + 9;	// ten, jack, queen, king have same value in blackjack, so we won't differentiate for the key
+		}
+	}
+
+	return key;
+}
+
+// handToIndex: finds the index in the hash table for a given hand
+int handToIndex (handType *hand) {
+	int index = hand->cards[0]->rank * 10 + hand->cards[1]->rank;	// we are guaranteed the hand will have at least two cards
+
+	int offset = hand->cards[0]->rank * (hand->cards[0]->rank + 1) / 2;	// this offset accounts for index values that
+															// are illegal due to the hands being sorted (e.g. 20, 32, 31)
+	return index - offset;
+}
+
+void hashTableInsert (hashTableType *table, handType *hand, int response) {
+	printf ("hashTableInsert\n\n");
+	int index = handToIndex(hand);
+	long long int key = handToKey(hand);
+	table->heads[index] = listInsert(table->heads[index], key, response);
+}
+
+void hashTableInitAllKeys (hashTableType *table, handType *hand, int* ranks, int curRank) {
+	printf ("hashTableInitAllKeys: ");
+	for (int i = 0; i < hand->handSize; i++) {
+		printf ("%d ", hand->cards[i]->rank);
+	}
+	printf ("\n");
+
+	//hashTableInsert (table, hand, randInt(0, 1));	// choose an initially random response for the AI
+
+	if (ranks[curRank] >= NUM_SUITS) {
+		curRank += 1;
+	}
+
+	for (int i = curRank; i < NUM_RANKS; i++) {
+		hand->cards[hand->handSize]->rank = i;
+		hand->handSize += 1;
+		ranks[i] += 1;
+
+		if (handFindSum (hand) > 21) {
+			hand->handSize -= 1;
+			ranks[i] -=1;
+			break;
+		}
+
+		hashTableInitAllKeys (table, hand, ranks, i);
+		hand->handSize -= 1;
+		ranks[i] -= 1;
+	}
+}
+
+void hashTableInit (hashTableType *table) {
+	printf ("hashTableInit\n");
+
+	handType hand;
+	int ranks[NUM_RANKS] = {0};
+
+	table = malloc (sizeof (hashTableType));
+	if (table == NULL) {
+		printf ("Memory allocation for hash table failed\n");
+	}
+
+	for (int i = 0; i < MAX_HAND_SIZE; i++) {
+		hand.cards[i] = malloc (sizeof (cardType));
+		if (hand.cards[i] == NULL) {
+			printf ("Memory allocation for cards in hand failed\n");
+		}
+	}
+
+	hand.handSize = 0;
+
+	for (int i = 0; i < NUM_RANKS - 1; i++) {
+		hand.cards[hand.handSize]->rank = i;
+		hand.handSize += 1;
+		ranks[i] += 1;
+
+		for (int j = i; j < NUM_RANKS; j++) {
+			hand.cards[hand.handSize]->rank = j;
+			hand.handSize += 1;
+			ranks[j] += 1;
+
+			hashTableInitAllKeys (table, &hand, ranks, j);
+
+			hand.handSize -= 1;
+			ranks[j] -= 1;
+		}
+
+		hand.handSize -=1;
+		ranks[i] -= 1;
+	}
+}
+
+/********************************************
+ 	 FUNCTIONS FOR THE AI
+ ********************************************/
+
+// calculateFitness: find the fitness of a given AI by seeing how many games it wins
+int calculateFitness (AIType *AI) {
+	handType dealer;
+	for (int i = 0; i < NUM_GAMES_PER_EVALUATION; i++) {
+
+
 	}
 }
 
 int main () {
-	cardType *deck[DECK_SIZE]; // make the deck to be an array of pointers so shuffling is more efficient
-	int deckIndex = 0;
-	handType player, dealer;
+
+	//cardType *deck[DECK_SIZE]; // make the deck to be an array of pointers so shuffling is more efficient
 
 	srand((unsigned int)time(NULL));
 
-	createDeck(deck);
-	shuffleDeck(deck);
-	//printDeck(deck);
+	//createDeck(deck);
+	//shuffleDeck(deck);
+	//printCards(deck, DECK_SIZE);
 
-	for (int i = 0; i < INITIAL_HAND_SIZE; i++) {
-		player.cards[i] = deck[deckIndex++];
+	/*
+	handType hand;
+	hand.handSize = 10;
+	for (int i = 0; i < hand.handSize; i++) {
+		hand.cards[i] = deck[i];
 	}
-	for (int i = 0; i < INITIAL_HAND_SIZE; i++) {
-		dealer.cards[i] = deck[deckIndex++];
-	}
+	printf ("%d %d\n\n", deck[5]->rank, deck[5]->suit);
+	handRemove(&hand, deck[5]);
+	printCards (hand.cards, hand.handSize);
+	*/
+
+	hashTableType table;
+	hashTableInit (&table);
 
 
+
+	return 0;
 
 }
 
